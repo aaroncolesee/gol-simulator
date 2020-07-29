94966b4f2cd292a1347195ab8d0b7cbfeaba528e
aaroncolesee <seeaaronc@gmail.com>
2020-07-29 12:48:00 +1000

    Updates

---
+++ precache-manifest.9302d4bdeaaf717f1ccb8d27fe698ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed6c60d777a759a696605697fabb78d",
+    "url": "/gol-simulator/index.html"
+  },
+  {
+    "revision": "adb9889afe1f9887e528",
+    "url": "/gol-simulator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d833d979ea1d91786f71",
+    "url": "/gol-simulator/static/js/2.42bb1d06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gol-simulator/static/js/2.42bb1d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb9889afe1f9887e528",
+    "url": "/gol-simulator/static/js/main.3eba6f38.chunk.js"
+  },
+  {
+    "revision": "1cce2dd21e3de6cb873f",
+    "url": "/gol-simulator/static/js/runtime-main.84656c32.js"
+  }
+]);
